354bbc8d9486fb49d122f898000266d8d77eb60e
Fernando Ramos <104237769+Fernandorsoul@users.noreply.github.com>
2022-05-12 19:11:35 -0300

    Projeto de abstração
    primeiros passos usando abstração no c#

---
+++ src/entities/Wizard.cs
@@ -0,0 +1,13 @@
+namespace abstração_rpg.entities
+{
+    public class Wizard:Hero{
+        public Wizard(string Name,int Level,string HeroType){
+            this.Name=Name;
+            this.Level=Level;
+            this.HeroType=HeroType;
+        }
+        public override string Attack(){
+             return this.Name+ " Lançou uma magia!";
+             }
+    }    
+}
